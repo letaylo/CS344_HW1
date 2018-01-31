d24534242d2dfd1fcab4f160b030aea34a80f24d
nieterde <35738629+nieterde@users.noreply.github.com>
2018-01-31 14:13:24 -0500

    Add files via upload

--- iterative_search.cpp
+++ iterative_search.cpp
@@ -1,21 +1,51 @@
-#include <stdlib.h>
-#include <assert.h>
-#include <iostream>
-#include <fstream>
-
 void iterative_search(int buffer[], int size, int& min, int& max, int& cost)
 {
-   for(int i = 0; i < size; i++)
-   {
+  int start;
+  if(size % 2 == 0)
+  {
+    start = 2;
+    if(buffer[0] < buffer[1])
+    {
+      min = buffer[0];
+      max = buffer[1];
+    }
+    else
+    {
+      min = buffer[1];
+      max = buffer[0];
+    }
+    cost++;
+  }
+  else
+  {
+    start = 1;
+    min = buffer[0];
+    max = buffer[1];
+  }
+  for(int i = start; i < size; i = i + 2)
+  {
+    if(buffer[i] < buffer[i+1])
+    {
       if(buffer[i] < min)
       {
-         min = buffer[i];
+        min = buffer[i];
       }
-      cost++;
+      if (buffer[i+1] > max)
+      {
+        max = buffer[i+1];
+      }
+    }
+    else
+    {
       if(buffer[i] > max)
       {
-         max = buffer[i];
+        max = buffer[i];
       }
-      cost++;
-   }
+      if(buffer[i+1] < min)
+      {
+        min = buffer[i+1];
+      }
+    }
+    cost += 3;
+  }       
 }

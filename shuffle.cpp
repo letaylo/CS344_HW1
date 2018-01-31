7a837ce433ee782a1b57295885bf3d836b8a79a1
nieterde <35738629+nieterde@users.noreply.github.com>
2018-01-31 14:20:59 -0500

    Add files via upload

--- shuffle.cpp
+++ shuffle.cpp
@@ -1,18 +1,10 @@
-#include <stdlib.h>
-#include <assert.h>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-
 void shuffle(int arr[], int size)
 {
    srand(time(0));
-   int temp;
-   for(int i = 0; i < size; i++)
+   int randLoc;
+   for(int i = size; i > 0; i--)
    {
-      int randLoc = rand() % size;
-      temp = arr[i];
-      arr[i] = arr[randLoc];
-      arr[randLoc] = temp;
+      randLoc = rand() % size;
+      swap(arr[i], arr[randLoc]);
    }
 }

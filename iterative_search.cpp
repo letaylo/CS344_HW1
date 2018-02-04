6e900b7c2882ffe002411a60441a617bfbf96963
nieterde <35738629+nieterde@users.noreply.github.com>
2018-02-04 12:28:16 -0500

    Update iterative_search.cpp

--- iterative_search.cpp
+++ iterative_search.cpp
@@ -1,51 +1,59 @@
 void iterative_search(int buffer[], int size, int& min, int& max, int& cost)
 {
   int start;
-  if(size % 2 == 0)
+  if(size == 1)
   {
-    start = 2;
-    if(buffer[0] < buffer[1])
-    {
-      min = buffer[0];
-      max = buffer[1];
-    }
-    else
-    {
-      min = buffer[1];
-      max = buffer[0];
-    }
-    cost++;
+    min = buffer[0];
+    max = buffer[0];
   }
   else
   {
-    start = 1;
-    min = buffer[0];
-    max = buffer[1];
-  }
-  for(int i = start; i < size; i = i + 2)
-  {
-    if(buffer[i] < buffer[i+1])
+    if(size % 2 == 0)
     {
-      if(buffer[i] < min)
+      start = 2;
+      if(buffer[0] < buffer[1])
       {
-        min = buffer[i];
+        min = buffer[0];
+        max = buffer[1];
       }
-      if (buffer[i+1] > max)
+      else
       {
-        max = buffer[i+1];
+        min = buffer[1];
+        max = buffer[0];
       }
+      cost++;
     }
     else
     {
-      if(buffer[i] > max)
-      {
-        max = buffer[i];
-      }
-      if(buffer[i+1] < min)
-      {
-        min = buffer[i+1];
-      }
+      start = 1;
+      min = buffer[0];
+      max = buffer[1];
     }
-    cost += 3;
-  }       
+    for(int i = start; i < size; i = i + 2)
+    {
+      if(buffer[i] < buffer[i+1])
+      {
+        if(buffer[i] < min)
+        {
+          min = buffer[i];
+        }
+        if (buffer[i+1] > max)
+        {
+          max = buffer[i+1];
+        }
+      }
+      else
+      {
+        if(buffer[i] > max)
+        {
+          max = buffer[i];
+        }
+        if(buffer[i+1] < min)
+        {
+          min = buffer[i+1];
+        }
+      }
+      cost += 3;
+    }       
+  }
 }
